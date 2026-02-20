3e27cb89c2e9b8adb67ebbb64f871e68b6d96bd2
Luca Giampieri <luca.giampieri95@gmail.com>
2026-02-20 14:59:15 +0100

    Settaggio iniziale DB e middlewares

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT;
+
+
+// import del middelware di gestione errore interno 500
+const errorsHandler = require("./middlewares/errorsHandler");
+
+// import del middelware di gestione di rotta inesistente
+const notFound = require("./middlewares/notFound");
+
+// rotta home APP
+app.get('/api', (req, res) => {
+    res.send("<h1>Rotta di home della nostra App di film</h1>")
+})
+
+
+// registriamo middelware di gestione err 500
+app.use(errorsHandler);
+
+// registriamo middelware di gestione rotta inesistente
+app.use(notFound);
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
